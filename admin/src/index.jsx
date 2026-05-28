c9af82eeaa2198d49b773c999abe46b18c0eaee3
disaster123 <stefan@prie.be>
2026-05-28 12:35:28 +0200

    fix(admin): wrap ThemeProvider with StyledEngineProvider in index

--- admin/src/index.jsx
+++ admin/src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
+import { StyledEngineProvider, ThemeProvider } from "@mui/material/styles";
 import { Theme } from "@iobroker/adapter-react-v5";
 import { Utils } from "@iobroker/adapter-react-v5";
 import App from "./app";
@@ -10,25 +10,27 @@ let themeName = Utils.getThemeName();
 
 function build() {
     ReactDOM.render(
-        <MuiThemeProvider theme={Theme(themeName)}>
-            {window.location.pathname.endsWith("/tab_m.html") ? (
-                <TabApp
-                    adapterName="logsearch"
-                    onThemeChange={(_theme) => {
-                        themeName = _theme;
-                        build();
-                    }}
-                />
-            ) : (
-                <App
-                    adapterName="logsearch"
-                    onThemeChange={(_theme) => {
-                        themeName = _theme;
-                        build();
-                    }}
-                />
-            )}
-        </MuiThemeProvider>,
+        <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={Theme(themeName)}>
+                {window.location.pathname.endsWith("/tab_m.html") ? (
+                    <TabApp
+                        adapterName="logsearch"
+                        onThemeChange={(_theme) => {
+                            themeName = _theme;
+                            build();
+                        }}
+                    />
+                ) : (
+                    <App
+                        adapterName="logsearch"
+                        onThemeChange={(_theme) => {
+                            themeName = _theme;
+                            build();
+                        }}
+                    />
+                )}
+            </ThemeProvider>
+        </StyledEngineProvider>,
         document.getElementById("root"),
     );
 }
